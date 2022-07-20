792920832c9e213675acbc9921f9ae9ca84fbd04
manavshah <manavshah1011.ms@gmail.com>
2022-07-21 03:10:26 +0530

    your orders for guest user

--- orders/views.py
+++ orders/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render,get_object_or_404
 from . models import Order
 from django.http import HttpRequest, JsonResponse
 from billing.models import BillingProfile
+from accounts.models import GuestEmail
 
 # Create your views here.
 
 def OrderObjects(request):
-    billing_profile_obj=BillingProfile.objects.get(user=request.user)
-    object=Order.objects.filter(billing_profile=billing_profile_obj)
+    if request.user.is_authenticated:
+        billing_profile_obj=BillingProfile.objects.get(user=request.user)
+        object=Order.objects.filter(billing_profile=billing_profile_obj)
+    else:
+        guest_obj=GuestEmail.objects.get(id=request.session.get('guest_email_id'))
+        billing_profile_obj=BillingProfile.objects.get(email=guest_obj.email)
+        object=Order.objects.filter(billing_profile=billing_profile_obj)
     context={
         'object':object,
     }
