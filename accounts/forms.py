b63d7c4e6aef980c4597edd2473c98bac3737839
manavshah <manavshah1011.ms@gmail.com>
2022-07-21 02:55:12 +0530

    signup_trial_2

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,19 @@ from django.contrib.auth.forms import  UserCreationForm
 from . import models
 from django.contrib.auth import get_user_model
 
-class SignUpForm(UserCreationForm):
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+class SignUpForm(UserCreateForm):
     class Meta:
         model=get_user_model()
         fields=('username','email','password1','password2')
-        
 
     
 class GuestForm(forms.ModelForm):
