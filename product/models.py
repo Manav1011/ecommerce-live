eb40e0c74b6594d80d28d3842120b83e43ed6192
manavshah <manavshah1011.ms@gmail.com>
2022-07-21 21:11:09 +0530

    image fix

--- product/models.py
+++ product/models.py
@@ -60,7 +60,7 @@ class Product(models.Model):
     description=models.TextField()
     price=models.DecimalField(decimal_places=2
                               ,max_digits=10,default=39.99)
-    image=models.ImageField(upload_to=upload_image_path,null=True,blank=True)
+    image=models.CharField(max_length=255,blank=True,null=True)
     slug=models.SlugField(blank=True,unique=True)
     featured=models.BooleanField(default=False)
     active=models.BooleanField(default=True)
