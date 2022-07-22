a300dc88cda3499c72c91de0878d35dbca50929c
manavshah <manavshah1011.ms@gmail.com>
2022-07-22 19:52:30 +0530

    slight more correction

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -28,9 +28,10 @@ from carts.views import cart_detail_api_view
 from search.views import getProducts
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     re_path('^$',ecommerceview.HomeView.as_view(),name='home'),
     re_path('^contact/$',ecommerceview.ContactView,name='contact'),
-    re_path(r'^login/$',LoginView.as_view(template_name='login.html'),name='login'),
+    re_path(r'^login/$',login_view,name='login'),
     re_path(r'^logout/$',LogoutView.as_view(),name='logout'),
     path('accounts/',include('accounts.urls')),
     path('orders/',include('orders.urls')),
